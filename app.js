b891fc814f787424c012788bbc78d6974e80bfed
Dalton-Dias <112430092+Dalton-Dias@users.noreply.github.com>
2022-12-26 14:12:28 -0300

    Teste

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicacação está se conectando os Banco de Dados")
 console.log("Enviando para o GitHub")
+console.log("Teste")
