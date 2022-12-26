4f23de260baed29cc5b78e7b7c1097f826717e02
Dalton Dias <daltonordias@gmail.com>
2022-12-26 14:27:43 -0300

    Voltando

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicacação está se conectando os Banco de Dados")
 console.log("Enviando para o GitHub")
-console.log("Teste")
